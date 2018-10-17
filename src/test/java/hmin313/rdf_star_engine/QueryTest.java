0015cfed3f06cc0b9c58a0ba3ac61ff871cc63b0
Florian PASCUAL <35746503+florianpascualsoawesome@users.noreply.github.com>
2018-10-17 12:28:25 +0200

    Starting tests with querying

---
+++ src/test/java/hmin313/rdf_star_engine/QueryTest.java
@@ -0,0 +1,79 @@
+package hmin313.rdf_star_engine;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import static org.junit.Assert.assertEquals;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import dictionary.Dictionary;
+import index.Index;
+import index.OPSIndex;
+import index.POSIndex;
+
+public class QueryTest {
+	
+	/*@BeforeClass
+	public static void setup() throws RDFParseException, RDFHandlerException, IOException {
+	
+	}*/
+	
+		Dictionary dico;
+	
+		@BeforeClass
+	public static void setup() throws RDFParseException, RDFHandlerException, IOException {
+		System.out.println("Reading data [Start]");	
+		Instant t1 = Instant.now();
+		Reader reader = new FileReader("data/500K.rdfxml");
+		RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
+		Stored_RDFListener listenner = new Stored_RDFListener();
+		rdfParser.setRDFHandler(listenner);
+		rdfParser.parse(reader, "");
+		reader.close();
+		termList = listenner.getTerms().keySet();
+		int totalSize = 0;
+		for(String term : termList) {
+			totalSize += term.length();
+		}
+		System.out.println("\tDataset size:"+totalSize+"B\n");
+		System.out.println("\tReading time="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
+
+		totalSize = 0;
+		dico = new Dictionary(listenner.getTerms());
+		
+	
+	@Test
+	public void testDicoMatching(){
+	System.out.println("[START]");
+	Instant t1 = Instant.now();
+	queryMain("data/queries/Q_1_eligibleregion.queryset", dico, );
+	System.out.println("Matching "+triple_size+" ids\n"
+				+"\ttime="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
+	
+	}
+	
+		@Test
+	public void testQuerying(){
+	
+	Instant t1 = Instant.now();
+	
+	System.out.println("Reading "+triple_size+" triples\n"
+				+"\ttime="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
+	
+	}
+	
+}
