1d4e04141b6bd54b31200c8e7b22b10e4bc06c73
Florian PASCUAL <35746503+florianpascualsoawesome@users.noreply.github.com>
2018-10-17 17:49:40 +0200

    Add files via upload

--- src/test/java/hmin313/rdf_star_engine/QueryTest.java
+++ src/test/java/hmin313/rdf_star_engine/QueryTest.java
@@ -19,6 +19,7 @@ import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
+import Query.Query;
 import Dictionary.Dictionary;
 import Index.*;
 
@@ -30,43 +31,53 @@ public class QueryTest {
 	}*/
 	
 		public static Dictionary dico;
+		private static Collection<String> termList;
+		private static ArrayList<ArrayList<String>> triples;
+		private static Index posIndex, opsIndex;
 	
-//	@BeforeClass
-//	public static void setup() throws RDFParseException, RDFHandlerException, IOException {
-//		System.out.println("Reading data [Start]");	
-//		Instant t1 = Instant.now();
-//		Reader reader = new FileReader("data/500K.rdfxml");
-//		RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
-//		Stored_RDFListener listenner = new Stored_RDFListener();
-//		rdfParser.setRDFHandler(listenner);
-//		rdfParser.parse(reader, "");
-//		reader.close();
-//		
-//		System.out.println("\tDataset size:"+totalSize+"B\n");
-//		System.out.println("\tReading time="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
-//
-//		totalSize = 0;
-//		dico = new Dictionary(listenner.getTerms());
+	@BeforeClass
+	public static void setup() throws RDFParseException, RDFHandlerException, IOException {
+		System.out.println("Reading data [Start]");	
+		Instant t1 = Instant.now();
+		Reader reader = new FileReader("data/500K.rdfxml");
+		RDFParser rdfParser = Rio.createParser(RDFFormat.RDFXML);
+		Stored_RDFListener listenner = new Stored_RDFListener();
+		rdfParser.setRDFHandler(listenner);
+		rdfParser.parse(reader, "");
+		reader.close();
 		
+		System.out.println("\tDataset size:B\n");
+		System.out.println("\tReading time="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
+		dico = new Dictionary(listenner.getTerms());
+		
+		System.out.println("Reading data [Start]");	
+		
+		Instant t2 = Instant.now();
+		Reader reader2 = new FileReader("data/500K.rdfxml");
+		RDFParser rdfParser2 = Rio.createParser(RDFFormat.RDFXML);
+		Stored_RDFListener listenner2 = new Stored_RDFListener();
+		rdfParser2.setRDFHandler(listenner2);
+		rdfParser2.parse(reader2, "");
+		reader2.close();
+		termList = listenner.getTerms().keySet();
+		triples = listenner.getTriples();
+		int triple_size = triples.get(0).size();
+		
+		System.out.println("Reading triples\n"
+				+"\ttime="+Duration.between(t2,Instant.now()).toMillis()+"ms [OK]");
+		
+		}
 	
-//	@Test
-//	public void testDicoMatching(){
-//		System.out.println("[START]");
-//		Instant t1 = Instant.now();
-//		queryMain("data/queries/Q_1_eligibleregion.queryset", dico, );
-//		System.out.println("Matching "+triple_size+" ids\n"
-//					+"\ttime="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
-//	
-//	}
+	@Test
+	public void testQuerying(){
+		System.out.println("[START]");
+		Instant t1 = Instant.now();
+		Index ind = new OPSIndex(dico,triples);
+		String option = "terminal";
+		Query q = new Query("data/queries/Q_1_eligibleregion.queryset", dico, ind, option);
+		System.out.println("Matching ids\n"
+					+"\ttime="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
 	
-//		@Test
-//	public void testQuerying(){
-//	
-//			Instant t1 = Instant.now();
-//	
-//			System.out.println("Reading "+triple_size+" triples\n"
-//				+"\ttime="+Duration.between(t1,Instant.now()).toMillis()+"ms [OK]");
-//	
-//	}
+	}
 	
 }
